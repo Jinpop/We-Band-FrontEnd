587408b305f90e60588ed9578b169a37a4efa74a
jin <qktjwl123@gmail.com>
2025-11-20 15:08:21 +0900

    chore: path alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     port: 3000, //3000번 포트 사용
+  },
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@api": path.resolve(__dirname, "src/api"),
+      "@layout": path.resolve(__dirname, "src/layout"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@constants": path.resolve(__dirname, "src/constants"),
+      "@icons": path.resolve(__dirname, "src/icons"),
+      "@store": path.resolve(__dirname, "src/store"),
+    },
   },
   plugins: [react()],
 });
