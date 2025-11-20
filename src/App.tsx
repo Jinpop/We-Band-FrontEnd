f97a8bf94fa05a010c5f69e46379c542bf97b75b
jin <qktjwl123@gmail.com>
2025-11-20 16:01:20 +0900

    chore: nested router 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Layout from '@layout/Layout';
+import Home from '@pages/Home';
 
 function App() {
   return (
-    <>
-      <h1>We Band</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   server: {
     port: 3000, //3000번 포트 사용
