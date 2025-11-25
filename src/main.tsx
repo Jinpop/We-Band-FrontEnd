c547d4cba85d7a078627f66a92c7a326adc0c3bd
jin <qktjwl123@gmail.com>
2025-11-25 14:49:15 +0900

    chore: 디자인 시스템 구축

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,13 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import '@styles/global/global.scss';
 
+import { ThemeProvider } from 'styled-components';
+import theme from '@styles/theme/theme.ts';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
