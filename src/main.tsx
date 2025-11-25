82269f9477594fccd73750a3b7cbb45260f1e743
박성진 <qktjwl123@gmail.com>
2025-11-25 15:11:51 +0900

    Chore/3 design system (#4)
    * chore: sass, styled-component 다운
    
    * chore: 전역 디자인 초기화
    
    * chore: 디자인 시스템 구축

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+
 import App from './App.tsx';
+import '@styles/global/global.scss';
+
+import { ThemeProvider } from 'styled-components';
+import theme from '@styles/theme/theme.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
