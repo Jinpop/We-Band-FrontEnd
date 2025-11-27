d97d99a3936360ccb162a3d395c070da40957b04
jin <qktjwl123@gmail.com>
2025-11-27 19:12:43 +0900

    feat: 인가 코드 받아오기 및 redirect 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,22 @@ import Layout from '@layout/Layout';
 import Home from '@pages/Home';
 import * as S from './App.styled';
 import OnBoarding from '@pages/Onboarding';
+import ProtectedRoute from '@components/ProtectedRoute';
+import Auth from '@pages/Auth';
 
 function App() {
   return (
     <BrowserRouter>
       <S.AppContainer>
         <Routes>
-          <Route index element={<OnBoarding />} />
+          <Route path="onboarding" element={<OnBoarding />} />
+          <Route path="auth-callback" element={<Auth />} />
 
-          {/* <Route element={<Layout />}>
-            <Route index element={<Home />} />
-          </Route> */}
+          <Route element={<ProtectedRoute />}>
+            <Route element={<Layout />}>
+              <Route index element={<Home />} />
+            </Route>
+          </Route>
         </Routes>
       </S.AppContainer>
     </BrowserRouter>
